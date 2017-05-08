6c0d3652b385e589c80cd3cadb929aa80412a807
LAPTOP-QEIE98BA\Jakub <jakub.machacekk@gmail.com>
2017-05-08 11:42:29 +0200

    no message

--- firstYearProject/src/Main.java
+++ firstYearProject/src/Main.java
@@ -13,7 +13,6 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        System.out.println("Just testing new branch");
     }
 
 
