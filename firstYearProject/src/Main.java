2ed36e7a67f492220d11dbb88a50a5d337acf448
LAPTOP-QEIE98BA\Jakub <jakub.machacekk@gmail.com>
2017-05-08 11:38:23 +0200

    changing message(still testing)

--- firstYearProject/src/Main.java
+++ firstYearProject/src/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        System.out.println("Just testing commit");
+        System.out.println("Just testing new branch");
     }
 
 
