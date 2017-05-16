abb5d1134f0f277bb63a0dfaf7b0c7542ff27e3f
Rasmus <rasbalnor@gmail.com>
2017-05-16 23:41:12 +0200

    service tasks now working

--- firstYearProject/src/controller/ServiceController.java
+++ firstYearProject/src/controller/ServiceController.java
@@ -1,7 +1,48 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Depot;
+import model.Service;
+
 /**
  * Created by Jakub on 09.05.2017.
  */
 public class ServiceController {
+
+    private Service service = null;
+
+    public ObservableList<Service> loadServices ()
+    {
+        Depot depot = new Depot();
+
+        ObservableList<Service> services = FXCollections.observableArrayList();
+        services.addAll(depot.getServices());
+        return services;
+    }
+
+    public boolean updateService(int id, double kmCount,
+                                 boolean kmChecked, boolean enoughGas,
+                                 boolean noRepair, boolean cleaned)
+    {
+        service = new Service(id, kmCount, kmChecked,
+                enoughGas, noRepair, cleaned);
+
+        return service.update();
+    }
+
+    public boolean markDone(Service service)
+    {
+        return service.markDone();
+    }
+
+    public Service getService()
+    {
+        return service;
+    }
+
+    public void setService(Service service)
+    {
+        this.service = service;
+    }
 }
