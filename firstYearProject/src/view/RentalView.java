aa26aaeb3e75ceeabc5953f22ea3cf4436b2c168
Nikolay Kanchev <nikolaj.kanchev@gmail.com>
2017-05-18 12:32:09 +0200

    empty lines

--- firstYearProject/src/view/RentalView.java
+++ firstYearProject/src/view/RentalView.java
@@ -143,6 +143,8 @@ public class RentalView implements Initializable
 
     public void saveChanges(ActionEvent event)
     {
+
+        
         //coController.updateRental(selectedRental);
     }
 
